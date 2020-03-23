f67d1faa1d0994460b5fe21b129fa677b712eea9
Thomas Guillet <guillet.thomas@gmail.com>
2020-03-23 12:11:50 +0100

    fixup! Implémente un web hook

--- spec/models/rdv/send_web_hook_spec.rb
+++ spec/models/rdv/send_web_hook_spec.rb
@@ -10,7 +10,7 @@ describe Rdv, type: :model do
 
     describe 'default publication' do
       it 'publishes the default status' do
-        expect(WebHookJob).to receive(:perform_later).with(hash_including('status' => 'unknown'))
+        expect(WebHookJob).to receive(:perform_later).with(hash_including('status' => 'unknown'), instance_of(String))
         rdv.reload
       end
     end
@@ -19,13 +19,13 @@ describe Rdv, type: :model do
       before { rdv.reload }
 
       it 'publishes the new status' do
-        expect(WebHookJob).to receive(:perform_later).with(hash_including('status' => 'excused'))
+        expect(WebHookJob).to receive(:perform_later).with(hash_including('status' => 'excused'), instance_of(String))
         rdv.status = :excused
         rdv.save
       end
 
       it 'publishes the deleted status' do
-        expect(WebHookJob).to receive(:perform_later).with(hash_including('status' => 'deleted'))
+        expect(WebHookJob).to receive(:perform_later).with(hash_including('status' => 'deleted'), instance_of(String))
 
         rdv.destroy
       end
